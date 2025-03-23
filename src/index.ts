761d442591f4d36ee9dc5e597175b03069ff9adc
nancyromales19 <nancyromales16@gmail.com>
2025-03-23 16:18:01 +0800

    Creation of user

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import { AppDataSource } from "./ormconfig";
+import userRoutes from "./routes/user";
+
+const app = express();
+app.use(express.json());
+
+AppDataSource.initialize()
+    .then(() => {
+        console.log("Database connected");
+
+        app.use("/users", userRoutes);
+
+        app.listen(3000, () => console.log("Server running on port 3000"));
+    })
+    .catch((error) => console.log(error));
